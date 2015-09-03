afa8267b67256da116e8468555de5beac98fe293
Diego T. Fialho <diego@maxisite.net>
2015-09-03 17:16:04 -0300

    git init

---
+++ js/chat.js
@@ -0,0 +1,54 @@
+var array_colors = ['007AFF','FF7000','FF7000','15E25F','CFC700','CFC700','CF1100','CF00BE','F00'];
+var random = Math.floor((Math.random() * 10) + 1);
+
+$(document).ready(function(){
+	var wsUri = "ws://192.168.2.99:9000/demo/server.php";
+	websocket = new WebSocket(wsUri);
+	
+	websocket.onopen = function(ev) {
+		$('#message_box').append("<div class=\"system_msg\">Conectado!</div>");
+	}
+
+	$('#send-btn').click(function(){
+		var mymessage = $('#mensagem').val();
+		var myname = $('#nome').val();
+		
+		if(myname == ""){
+			alert("Digite o seu nome!");
+			return;
+		}
+		if(mymessage == ""){
+			alert("Digite uma mensagem!");
+			return;
+		}
+		
+		var msg = {
+			message: mymessage,
+			name: myname,
+			color : '#'+array_colors[random]
+		};
+		websocket.send(JSON.stringify(msg));
+	});
+	
+	websocket.onmessage = function(ev) {
+		var msg = JSON.parse(ev.data);
+		var type = msg.type;
+		var umsg = msg.message;
+		var uname = msg.name;
+		var ucolor = msg.color;
+
+		if(type == 'usermsg') 
+		{
+			$('#message_box').append("<div><span class=\"user_name\" style=\"color:#"+ucolor+"\">"+uname+"</span> : <span class=\"user_message\">"+umsg+"</span></div>");
+		}
+		if(type == 'system')
+		{
+			$('#message_box').append("<div class=\"system_msg\">"+umsg+"</div>");
+		}
+		
+		$('#mensagem').val('');
+	};
+	
+	websocket.onerror	= function(ev){$('#message_box').append("<div class=\"system_error\">Ocorreu um erro - "+ev.data+"</div>");}; 
+	websocket.onclose 	= function(ev){$('#message_box').append("<div class=\"system_msg\">Conexão encerrada!</div>");}; 
+});
